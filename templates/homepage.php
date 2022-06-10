c18c94872163ab1fed11541dc5eb5006e2c745c2
ChristopherSemard <christophersemard76@gmail.com>
2022-06-10 09:43:00 +0200

    style message

--- templates/homepage.php
+++ templates/homepage.php
@@ -13,7 +13,7 @@
                 else{
                     $messages = array_reverse($messages);
                     foreach ($messages as $key => $message) {
-                        echo '<div class="message bg-dark d-flex p-2 my-2" style="border-bottom : 2px solid ' .$message->color.'">
+                        echo '<div class="message bg-dark rounded d-flex p-2 my-2" style="border-bottom : 2px solid ' .$message->color.'">
                                     <p class="my-auto"><span style=" color:' .$message->color. '">' .$message->pseudo. '</span> - ' .$message->date. '<br> ' .$message->text. '</p>
                                 </div>';
                     }
